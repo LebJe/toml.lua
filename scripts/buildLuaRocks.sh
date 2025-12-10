8d73881e13a5caecabe3ea909ad9150385c15bbd
LebJe <51171427+LebJe@users.noreply.github.com>
2025-12-10 17:50:13 -0500

    Update luarocks in CI script

--- scripts/buildLuaRocks.sh
+++ scripts/buildLuaRocks.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt install build-essential libreadline-dev unzip wget
-wget https://luarocks.org/releases/luarocks-3.11.0.tar.gz
-tar -zxpf luarocks-3.11.0.tar.gz
-rm luarocks-3.11.0.tar.gz
-cd luarocks-3.11.0
+sudo apt install build-essential libreadline-dev unzip wget make -yq
+wget "https://luarocks.org/releases/luarocks-3.12.2.tar.gz"
+tar -zxpf luarocks-3.12.2.tar.gz
+rm luarocks-3.12.2.tar.gz
+cd luarocks-3.12.2
 ./configure
 make
 sudo make install
