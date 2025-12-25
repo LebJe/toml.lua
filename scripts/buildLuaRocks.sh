cdf4b8350c0fd310166b0783cae186ab26ad8c2b
LebJe <51171427+LebJe@users.noreply.github.com>
2025-12-25 13:21:28 -0500

    insert path directly

--- scripts/buildLuaRocks.sh
+++ scripts/buildLuaRocks.sh
@@ -1,7 +1,6 @@
 
 case $(uname -v) in
     *FreeBSD*)
-		export LUA_BIN_PATH=/usr/local/bin/
 		sudo pkg install -y unzip wget gmake readline curl
         ;;
     *)
@@ -15,7 +14,7 @@ rm luarocks-3.12.2.tar.gz
 cd luarocks-3.12.2
 case $(uname -v) in
     *FreeBSD*)
-		./configure --with-lua-bin $LUA_BIN_PATH
+		./configure --with-lua-bin "usr/local/bin"
         ;;
     *)
 		./configure
